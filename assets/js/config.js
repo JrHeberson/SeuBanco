3ffa9e1bdb51afb885873ab741dd2ae447431b58
hrezende <heberson.rezende@worldline.com>
2021-05-24 14:18:37 -0300

    tentativa de cadastro de user

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_8UTHPZ945', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1je0sj1k4vfsjdshn5o8lia5hu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
